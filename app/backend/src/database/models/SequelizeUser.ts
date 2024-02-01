23af449aa988af24f530360d3a13dd3b75350eb1
cleitson <cleitson.ftw@gmail.com>
2024-02-01 19:16:48 -0300

    database de users/ completando toda a database

---
+++ app/backend/src/database/models/SequelizeUser.ts
@@ -0,0 +1,48 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeUser extends Model<InferAttributes<SequelizeUser>,
+InferCreationAttributes<SequelizeUser>> {
+  declare id: CreationOptional<number>;
+  declare username: CreationOptional<string>;
+  declare role: CreationOptional<string>;
+  declare email: CreationOptional<string>;
+  declare password: CreationOptional<string>;
+}
+
+SequelizeUser.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default SequelizeUser;
